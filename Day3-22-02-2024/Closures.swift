66fecdb6f67b620e4fe6cb050b2fb98c683de0d8
kaps2002 <karpandey2002@gmail.com>
2024-02-22 17:35:57 +0530

    Added Day3 Work

---
+++ Day3-22-02-2024/Closures.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+//Closures
+
+struct Student{
+  let name:String
+  let testscore:Int
+}
+
+let students=[Student(name:"Karan",testscore:89),Student(name:"Aakash",testscore:78),Student(name:"Vaibhav",testscore:65),Student(name:"Jatin",testscore:85),Student(name:"Louis",testscore:75),Student(name:"Sunil",testscore:62)]
+
+var topstudentsfilter: (Student) -> Bool = {student  in
+  return student.testscore >= 75
+}
+
+let topstudents = students.filter(topstudentsfilter)
+
+for i in topstudents{
+  print(i.name)
+}
+
+
+//Example2
+
+var comparetwonum: (Int,Int)->String={num1,num2 in
+  if(num1>num2){
+    return "\(num1) is great"
+  }
+  else{
+    return "\(num2) is great"
+  }
+}
+print(comparetwonum(2,4))
---
+++ Day3-22-02-2024/Properties&Methods.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+//Properties and Methods
+
+struct Person{
+  var name:String
+  var age: Int
+  var address:String
+
+  func describe () -> String{
+    return "I am \(name). I live in \(address) and \(age) yrs old"
+  }
+}
+
+var user1 = Person(name:"Karan",age:21,address:"Delhi")
+print(user1.name,user1.address)  //Properties
+print(user1.describe()) //Methods
+
+var user2 = Person(name:"Aakash",age:23,address:"Noida")
+print(user2.name,user2.address)
+print(user2.describe())
+
+//Property Observers
+
+class countsteps{
+  var totalsteps:Int=0{
+    willSet(newtotalsteps){
+      print("About to set totalSteps to \(newtotalsteps)")
+    }
+    didSet{
+      print("\(totalsteps+oldValue) is total steps")
+    }
+  }
+}
+
+var steps = countsteps()
+steps.totalsteps=200
+steps.totalsteps=200
+
+
+//Methods
+
+struct cuboid{
+  var a:Int;
+  var b:Int;
+  var c:Int;
+
+  func area()->Int{
+    return self.a * self.b * self.c  //Uses self keyword
+  }
+}
+
+var cube1=cuboid(a:23,b:23,c:23)
+var cuboid1=cuboid(a:10,b:12,c:14)
+print(cube1.area())
+print(cuboid1.area())
