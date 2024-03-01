991486aa30b3aa4ea9b23f350963b770e893d64c
kaps2002 <karpandey2002@gmail.com>
2024-03-01 22:16:21 +0530

    Corrections in Question3

--- Day8-29-02-2024/Question3.swift
+++ Day8-29-02-2024/Question3.swift
@@ -1,37 +1,90 @@
 import Foundation
 
-// Question 5
+// Question 3
 
-extension String {
-    func capitalizeFirstChar() -> String {
-        let str: String = self.dropFirst().lowercased()
-        return self.first!.uppercased() + str
-    }
+struct Student {
+    let name: String
+    let rollNo: Int
+    let dob: String
+    let bloodGroup: String
     
-    func capitalizeFirstCharOfSentence() -> String {
-        let strArr: [String] = self.components(separatedBy: " ")
-        var ans: String = ""
-        for word in strArr {
-            ans += word.first!.uppercased() + word.dropFirst().lowercased() + " "
+    func filterBlood(_ tempGroup: String, _ student: Student) -> Bool {
+        if tempGroup == "O+" || tempGroup == "A+" {
+            return true
         }
-        return ans
+        return false
     }
     
-    func isPalindrome() -> Bool {
-        let revStr: String = String(self.reversed())
-        if revStr == self {
+    func filterDOB(_ tempDOB: String, _ student: Student) -> Bool {
+        if tempDOB.contains("Jan") || tempDOB.contains("Dec") {
             return true
         }
         return false
     }
 }
 
-print("Malayalam".isPalindrome())
-print("Karan".isPalindrome())
-print("SWIFTUI".capitalizeFirstChar())
-print("ramesh".capitalizeFirstChar())
-let sentence: String = """
-SwiftUI is a modern way to declare user interfaces for any Apple platform. 
-Create beautiful, dynamic apps faster than ever before.
-"""
-print(sentence.capitalizeFirstCharOfSentence())
+let students = [
+    Student(name: "Aon", rollNo: 2, dob: "20-Dec-2003", bloodGroup: "O+"),
+    Student(name: "Max", rollNo: 29, dob: "18-Jan-2003", bloodGroup: "A+"),
+    Student(name: "Ron", rollNo: 45, dob: "20-Dec-2002", bloodGroup: "A-"),
+    Student(name: "Sonny", rollNo: 21, dob: "20-July-2003", bloodGroup: "A+"),
+    Student(name: "Kathy", rollNo: 34, dob: "20-June-2003", bloodGroup: "A-")
+]
+
+func filterNames(_ students: [Student]) {
+    var ans: [Student] = []
+    for student in students {
+        let firstChar: Character = student.name.first!
+        if firstChar == "A" || firstChar == "R" {
+            ans.append(student)
+        }
+    }
+    print("The details of students whose name starts with A and R are: ")
+    for student in ans {
+        print("\(student.name) \(student.rollNo) \(student.dob) \(student.bloodGroup)")
+    }
+}
+
+var ansBloodGroup: [Student] = []
+for student in students {
+    let tempGroup: String = student.bloodGroup
+    if student.filterBlood(tempGroup, student) {
+        ansBloodGroup.append(student)
+    }
+}
+print("The details of students whose BloodGroup equals O+ and A+ are: ")
+for student in ansBloodGroup {
+    print("\(student.name) \(student.rollNo) \(student.dob) \(student.bloodGroup)")
+}
+print("\n")
+
+func filterRollNo(_ students: [Student]) {
+    var ans: [Student] = []
+    for student in students {
+        let tempRoll: Int = student.rollNo
+        if tempRoll > 20 && tempRoll < 40 {
+            ans.append(student)
+        }
+    }
+    print("The details of students whose rollNo is between 20 and 40 are:")
+    for student in ans {
+        print("\(student.name) \(student.rollNo) \(student.dob) \(student.bloodGroup)")
+    }
+}
+
+var ansDOB: [Student] = []
+for student in students {
+    let tempDOB: String = student.dob
+    if student.filterDOB(tempDOB, student) {
+        ansDOB.append(student)
+    }
+}
+print("The details of students whose DOB equals Jan and Dec are: ")
+for student in ansDOB {
+    print("\(student.name) \(student.rollNo) \(student.dob) \(student.bloodGroup)")
+    
+}
+print("\n")
+filterNames(students)
+print("\n")
+filterRollNo(students)
